d3b0743466d9c5bb05f35c76ce9e27f07ff89b1c
Usman Riaz <usmanri4z@gmail.com>
2018-02-09 03:24:11 +0500

    working on stat updates

--- app/scenes/main-scene.ts
+++ app/scenes/main-scene.ts
@@ -1,5 +1,6 @@
 import {
     OnInit,
+    OnDestroy,
     ViewChild,
     Component,
 } from '@angular/core';
@@ -10,30 +11,42 @@ import {
 } from 'ionic-angular';
 import tap from 'lodash/tap';
 import { MainMenu } from './menu/main';
+import { Subject } from 'rxjs/Subject';
+import { GameStats } from '../models/stats';
 import { StatsStore } from '../store/stats.store';
 import { SettingsStore } from '../store/settings.store';
+import { combineLatest } from 'rxjs/observable/combineLatest';
 import { GameTimerComponent } from '../components/game-timer.component';
 import { SequenceGeneratorService } from '../services/sequence-generator.service';
+
+import 'rxjs/add/operator/do';
 
 @Component({
     selector: 'main-scene',
     templateUrl: 'main-scene.html'
 })
-export class MainScene implements OnInit {
+export class MainScene implements OnInit, OnDestroy {
     @ViewChild(GameTimerComponent) timer;
 
+    readonly lastRow: number = 1;
+
     rows: number[];
-    theme: string = 'defualt';
+    stats: GameStats;
+    difficulty: string;
     activeRow: number = 10;
-    currentColor: string | null;
     reset: boolean = false;
     running: boolean = false;
+    currentColor: string | null;
+
+    sceneDestroyed$: Subject<boolean> = new Subject<boolean>();
 
     constructor(
         public events: Events,
+        public statsStore: StatsStore,
         public navCtrl: NavController,
+        public settingsStore: SettingsStore,
+        public modalContrller: ModalController,
         public sequenceGenerator: SequenceGeneratorService,
-        public modalContrller: ModalController
     ) {
         this.rows = this.fillRows();
         this.events.subscribe('game:quit', _ => {
@@ -44,6 +57,19 @@ export class MainScene implements OnInit {
 
     ngOnInit() {
         this.showMenu();
+
+        combineLatest(
+            this.statsStore.store$,
+            this.settingsStore.difficulty$
+        )
+        .do(([stats, difficulty]: [GameStats, string]) => this.difficulty = difficulty)
+        .map(([stats, difficulty]: [GameStats, string]) => stats[difficulty])
+        .takeUntil(this.sceneDestroyed$)
+        .subscribe(stats => this.stats = stats);
+    }
+
+    ngOnDestroy() {
+        this.sceneDestroyed$.next(true);
     }
 
     setUp() {
@@ -76,7 +102,20 @@ export class MainScene implements OnInit {
     }
 
     update(event) {
-        if (event === false) this.activeRow--;
+        if(event === true) {
+            this.gameWon();
+        } else if (this.activeRow > this.lastRow) {
+            this.activeRow--;
+        } else {
+            this.gameOver();
+        }
+    }
+
+    gameWon() {
+
+    }
+
+    gameOver() {
 
     }
 
